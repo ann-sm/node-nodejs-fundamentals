8068dd425241227574e99e4c7fb64cbb05d85b60
ann-sm <annsmirnovva@gmail.com>
2026-03-08 00:15:11 +0300

    feat: complete fs tasks

--- src/fs/merge.js
+++ src/fs/merge.js
@@ -1,8 +1,52 @@
+import { promises as fs } from 'fs'; 
+import path from 'path'; 
+import { fileURLToPath } from 'url';
+
 const merge = async () => {
-  // Write your code here
-  // Default: read all .txt files from workspace/parts in alphabetical order
-  // Optional: support --files filename1,filename2,... to merge specific files in provided order
-  // Concatenate content and write to workspace/merged.txt
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+  
+    const workspaceDir = path.resolve(__dirname, '../../workspace');
+    const partsDir = path.join(workspaceDir, "parts");
+    const outputFile = path.join(workspaceDir, "merged.txt");
+
+    try {
+      await fs.access(partsDir);
+
+      const files = await fs.readdir(partsDir);
+
+      const args = process.argv;
+      const filesIndex = args.indexOf("--files");
+
+      let filesList = [];
+
+      if (filesIndex !== -1 && args[filesIndex + 1]) {
+        filesList = args.slice(filesIndex + 1).join().split(',').map(file => file.trim()).filter(file => path.extname(file));
+      } else {
+        filesList = files.filter(file => path.extname(file) === '.txt').sort();
+      }
+
+      let mergedContent = '';
+
+      for (const file of filesList) {
+        const filePath = path.join(partsDir, file);
+
+        try {
+          if (path.extname(file) !== '.txt') {
+            throw new Error;
+          }
+          const content = await fs.readFile(filePath, 'utf-8');
+          mergedContent = `${mergedContent}\n${content}`;
+        } 
+        catch {
+          throw new Error;
+        }
+      }
+      await fs.writeFile(outputFile, mergedContent);
+    }
+    catch {
+      console.error('FS operation failed');
+     }
 };
 
 await merge();
--- src/fs/restore.js
+++ src/fs/restore.js
@@ -1,8 +1,46 @@
+import { promises as fs } from 'fs'; 
+import path from 'path'; 
+import { fileURLToPath } from 'url';
+
 const restore = async () => {
-  // Write your code here
-  // Read snapshot.json
-  // Treat snapshot.rootPath as metadata only
-  // Recreate directory/file structure in workspace_restored
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const snapshotPath = path.resolve(__dirname, "../../snapshot.json");
+  const restoreDir = path.resolve(__dirname, "../../workspace_restored");
+
+  try {
+    await fs.access(snapshotPath);
+
+    try {
+      await fs.access(restoreDir);
+      throw new Error();
+    }
+    catch (error) {
+      if (error.code !== 'ENOENT') {
+        throw error;
+      }
+    }
+
+    const snapshotFile= await fs.readFile(snapshotPath, "utf-8");
+    const snapshot = JSON.parse(snapshotFile);
+
+    await fs.mkdir(restoreDir);
+
+    for (const entry of snapshot.entries) {
+      const entryPath = path.join(restoreDir, entry.path);
+
+      if (entry.type === 'directory') {
+        await fs.mkdir(entryPath, { recursive: true });
+      } else if (entry.type === 'file') {
+        const contentBuffer = Buffer.from(entry.content, 'base64');
+        await fs.writeFile(entryPath, contentBuffer);
+      }
+    }
+  }
+  catch {
+    console.error('FS operation failed');
+  }
 };
 
 await restore();
